7db8e05cb395a326261590d54ce59246e6e78e64
Vlad Bazhanov <bazhanvlad58@yandex.ru>
2019-01-24 12:14:55 +0300

    Creates page

--- js/main.js
+++ js/main.js
@@ -1,3 +1,3 @@
-import {one, two} from './modules/nums';
-
-document.write(`Сумма импортов: ${one + two}`);
+// import {one, two} from './modules/nums';
+//
+// document.write(`Сумма импортов: ${one + two}`);
